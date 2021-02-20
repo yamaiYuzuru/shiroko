53192ef6131089ca9cec402980ff4d95054912ab
yuzuru <44111695+yamaiYuzuru@users.noreply.github.com>
2021-02-20 11:44:06 +0100

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,5 +1,5 @@
 const {ShardingManager} = require('discord.js');
-const manager = new ShardingManager('./main.js', {token:"ODAzMzg3MzI4Mjk0MDI3MjY0.YA9CwQ.dp9vYCM8TPx1iVz_r16TLxbfic0"});
+const manager = new ShardingManager('./main.js', {token:"Your "});
 module.exports.start = () => {
     manager.spawn("auto", 15000, 10000000).catch(e => console.error(e));
     manager.on("shardCreate", (shard) => console.log(`Launching Shard ${shard.id}`));
