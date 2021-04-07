40eafec9ea50a068adbb11e5270ee72129928e99
yuzuru <44111695+yamaiYuzuru@users.noreply.github.com>
2021-04-07 14:33:04 +0200

    Update manager.js

--- manager.js
+++ manager.js
@@ -10,7 +10,7 @@ let {ShardingManager} = require('discord.js');
 module.exports = async () => {
     let manager = new ShardingManager('./main.js', {totalShards: "auto",
         shardList: "auto", mode: "process", respawn: true,
-        token: "ODAzMzg3MzI4Mjk0MDI3MjY0.YA9CwQ.24VkGwAfd8ZCfbfPUYbQORonVrI"});
+        token: "YOUR_TOKEN"});
 
     manager.on("shardCreate", async (shard) => {
         console.log(`[Shards] A shard with the id ${shard.id} was launched`);
