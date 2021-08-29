4327625558e9f96a26706e5e8354a3a8a19e0f31
yuzuru <44111695+yamaiYuzuru@users.noreply.github.com>
2021-08-29 19:36:00 +0200

    Create index.js

---
+++ configs/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: 'YOUR_BOT_TOKEN',
+    mongoDB_URI: 'YOUR_MONGODB URI',
+    admins: ['YOUR_DISCORD_USER_ID'],
+    dbl_token:'YOUR_TOP.GG_TOKEN',
+    dbl_auth: 'YOUR_WEBHOOK_AUTH',
+    bbl_token: "YOUR_BLADEBOTLIST_TOKEN",
+    webhook: 'YOUR_DISCORD_WEBHOOK'
+};
