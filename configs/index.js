8644e698eb6ca3bdfc6338179d74c85ce738e708
yuzuru <44111695+yamaiYuzuru@users.noreply.github.com>
2021-11-17 17:07:56 +0100

    Update index.js

--- configs/index.js
+++ configs/index.js
@@ -1,9 +1,16 @@
 module.exports = {
     token: 'YOUR_BOT_TOKEN',
     mongoDB_URI: 'YOUR_MONGODB URI',
+    testing_mongoDB_URI: 'mongodb://YOUR_MONGODB_TESTING_URI',
     admins: ['YOUR_DISCORD_USER_ID'],
     dbl_token:'YOUR_TOP.GG_TOKEN',
     dbl_auth: 'YOUR_WEBHOOK_AUTH',
     bbl_token: "YOUR_BLADEBOTLIST_TOKEN",
-    webhook: 'YOUR_DISCORD_WEBHOOK'
+    webhook: 'YOUR_DISCORD_WEBHOOK',
+    testing: false,
+    colors: {
+        red: "#cf2929",
+        green: "#64f551",
+        yellow: "#e7f551",
+    }
 };
