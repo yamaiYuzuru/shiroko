3b0d9aa2392dd26e48f608cdd2746e13354738fd
yuzuru <44111695+yamaiYuzuru@users.noreply.github.com>
2021-11-17 17:12:27 +0100

    Create Gif.js

---
+++ settings/Gif.js
@@ -0,0 +1,58 @@
+module.exports = class Gif {
+
+    static boop() {
+        return `https://shiroko.ml/cdn/boop/boop${Math.floor(Math.random() * 9)+1}.gif`;
+    };
+
+    static blush() {
+        return `https://shiroko.ml/cdn/blush/blush${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static cuddle() {
+        return `https://shiroko.ml/cdn/cuddle/cuddle${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static die() {
+        return `https://shiroko.ml/cdn/die/die${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static happy() {
+        return `https://shiroko.ml/cdn/happy/happy${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static hug() {
+        return `https://shiroko.ml/cdn/hug/hug${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static kill() {
+        return `https://shiroko.ml/cdn/kill/kill${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static kiss() {
+        return `https://shiroko.ml/cdn/kiss/kiss${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static mad() {
+        return `https://shiroko.ml/cdn/mad/mad${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static poke() {
+        return `https://shiroko.ml/cdn/poke/poke${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static shocked() {
+        return `https://shiroko.ml/cdn/shocked/shocked${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static shrug() {
+        return `https://shiroko.ml/cdn/shrug/shrug${Math.floor(Math.random() * 6)+1}.gif`;
+    }
+
+    static slap() {
+        return `https://shiroko.ml/cdn/slap/slap${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+
+    static smile() {
+        return `https://shiroko.ml/cdn/smile/smile0${Math.floor(Math.random() * 9)+1}.gif`;
+    }
+};
